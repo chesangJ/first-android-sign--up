bb89f3efd0860f796e71437158ad46693fbb0e4e
chesangJ <mitingijoychesang@gmail.com>
2023-04-26 22:30:26 +0300

    first commit

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.hello
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tvLogin:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvLogin=findViewById(R.id.tvlogin)
+        tvLogin.setOnClickListener {
+            val intent=Intent(this,loginActivity::class.java)
+        startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/hello/loginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class loginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
